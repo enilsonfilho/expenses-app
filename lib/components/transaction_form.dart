28d3e1540d852aebe0e5e4314e266bf4f90752d6
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-12-27 15:20:19 -0300

    feat: persist new transaction

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -5,6 +5,10 @@ class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
 
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit);
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -35,8 +39,9 @@ class TransactionForm extends StatelessWidget {
                         MaterialStateProperty.all<Color>(Colors.purple),
                   ),
                   onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0.0;
+                    onSubmit(title, value);
                   },
                 ),
               ],
--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:expenses/components/transaction_form.dart';
 import 'package:expenses/components/transaction_list.dart';
 import 'package:expenses/models/transaction.dart';
@@ -16,12 +17,25 @@ class _TransactionUserState extends State<TransactionUser> {
     Transaction(id: 't2', title: 'Conta de Luz', value: 211.30, date: DateTime.now())
   ];
 
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction (
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
+    return Column (
+      children: <Widget> [
         TransactionList(_transactions),
-        TransactionForm()
+        TransactionForm(_addTransaction),
       ],
     );
   }
