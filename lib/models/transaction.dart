d3c59801fb435c1eeb6850b50281f29e18cc0803
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-12-27 10:46:57 -0300

    feat: added model transaction

---
+++ lib/models/transaction.dart
@@ -0,0 +1,13 @@
+class Transaction {
+  final String id;
+  final String title;
+  final double value;
+  final DateTime date;
+
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.value,
+    required this.date,
+  });
+}
