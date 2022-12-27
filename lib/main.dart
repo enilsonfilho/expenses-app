78ff70b4bf48c5fb189e3a6cd0f5348f4f1eb245
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-12-27 10:44:30 -0300

    feat: added main and cross alignment

--- lib/main.dart
+++ lib/main.dart
@@ -21,9 +21,10 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas Pessoais!'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
-            width: double.infinity,
             child: Card(
               color: Colors.blue,
               child: Text('Gráfico'),
