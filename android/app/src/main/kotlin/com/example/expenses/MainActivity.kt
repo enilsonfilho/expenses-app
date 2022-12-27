be06fe27158e204f56654c4e197754fe49f90485
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-12-27 10:28:29 -0300

    feat: start project

---
+++ android/app/src/main/kotlin/com/example/expenses/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.expenses
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais!'),
+      ),
+      body: Center(
+        child: Text('Versão Inicial'),
+      ),
+    );
+  }
+}
