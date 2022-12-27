1c7296d5c76c4c66061f96be3978ebff1cde0b46
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-12-27 10:37:04 -0300

    feat: added graphic

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,20 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Despesas Pessoais!'),
       ),
-      body: Center(
-        child: Text('Versão Inicial'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('Lista de transações'),
+          )
+        ],
       ),
     );
   }
