b1b3b122e70a98bbb31c2aff4182732e52867441
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-12-28 10:01:14 -0300

    feat: close modal

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,8 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _transactions.add(newTransaction);
     });
+
+    Navigator.of(context).pop();
   }
 
   _openTransactionFormModal(BuildContext context) {
