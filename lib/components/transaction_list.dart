f0bf75bc8cc0f8c475d56b3aab2d44c213c1fdb4
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-12-29 16:18:01 -0300

    fix: height container list

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,7 +11,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 520,
       child: transactions.isEmpty ? Column(
         children: <Widget>[
           SizedBox(
