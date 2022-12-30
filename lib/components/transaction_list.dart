296e78b37775b2d50a53233d9656bd43ae9aa183
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-12-30 10:47:00 -0300

    fix: size image

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,65 +10,67 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return transactions.isEmpty ? Column(
-      children: <Widget>[
-        SizedBox(
-          height: 20,
-        ),
-        Text(
-          'Nenhuma transação cadastrada!',
-          style: Theme.of(context).textTheme.headline6,
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Container(
-          height: 200,
-          child: Image.asset(
-            'assets/images/waiting.png', 
-            fit: BoxFit.cover,
-          ),
-        ),
-      ],
-    ) : ListView.builder(
-      itemCount: transactions.length,
-      itemBuilder: (ctx, index) {
-        final tr = transactions[index];
-        return Card(
-          elevation: 5,
-          margin: EdgeInsets.symmetric(
-            vertical: 8,
-            horizontal: 5,
-          ),
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.purple,
-              foregroundColor: Colors.white,
-              radius: 30,
-              child: Padding(
-                padding: const EdgeInsets.all(6),
-                child: FittedBox(
-                  child: Text(
-                    'R\$${tr.value}'
+    return transactions.isEmpty
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Nenhuma transação cadastrada!',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ),
-            ),
-            title: Text(
-              tr.title,
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            subtitle: Text(
-              DateFormat('d MMM y').format(tr.date),
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.delete),
-              color: Colors.red[800],
-              onPressed: () => onRemove(tr.id),
-            ),
-          ),
-        );
-      },
-    );
+              ],
+            );
+          })
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              final tr = transactions[index];
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.purple,
+                    foregroundColor: Colors.white,
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Text('R\$${tr.value}'),
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    tr.title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    DateFormat('d MMM y').format(tr.date),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Colors.red[800],
+                    onPressed: () => onRemove(tr.id),
+                  ),
+                ),
+              );
+            },
+          );
   }
 }
